210c7e87bca33d01f4c8270a7bba91c64a21d3df
Drew Hontz <athontz@gmail.com>
2016-12-08 10:53:32 -0800

    Wow, this one took way longer than expected because I didn't sit and think about how to manage the last character.

---
+++ stringCompression2.java
@@ -0,0 +1,46 @@
+/* Clarify - No invalid inputs? Only strings with characters a-z A-Z?
+   Assuming that a != A? If there is not a repeat, do we concat 1 or just the letter?
+
+   Test Cases - empty string
+              - a string that actually gets replaced
+              - a string that has aA to test previous case
+              - a string that is not shorter when compressed
+
+    Brainstorm -
+
+    Runtime Analysis -
+
+ */
+
+import java.lang.StringBuilder;
+
+public class stringCompression2 {
+
+public static void main(String[] args) {
+        System.out.println("Expecting '' : " + stringCompress("")); // expecting ''
+        System.out.println("Expecting 'a1' : " + stringCompress("a")); // expecting ''
+        System.out.println("Expecting 'a3b2C5d1' : " + stringCompress("aaabbCCCCCd")); // expecting a3b2C5d1
+        System.out.println("Expecting 'a3b2C6' : " + stringCompress("aaabbCCCCCC")); // expecting a3b2C6
+        System.out.println("Expecting 'aabb' : " + stringCompress("aabb")); // expecting aabb
+}
+
+public static String stringCompress(String s) {
+        if (s.equals("")) return "Please provide a non empty string";
+        if (s.length() == 1) return s + 1;
+        StringBuilder answer = new StringBuilder();
+        int count = 1;
+        int i;
+        for (i = 1; i < s.length(); i++) {
+                if (s.charAt(i) == s.charAt(i - 1)) {
+                        count++;
+                } else {
+                        answer.append(s.charAt(i-1));
+                        answer.append(count);
+                        count = 1;
+                }
+
+        }
+        answer.append(s.charAt(i - 1)).append(count);
+        return (s.length() > answer.length()) ? answer.toString() : s;
+}
+}
